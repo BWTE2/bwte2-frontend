fc22bcc8c5427932f0d4cb75e48d32c3b6aa4349
matejPeluha <matej.peluha@gmail.com>
2021-05-06 09:18:08 +0200

    ADD: looping question creators and setting question type to json

--- src/app/components/lecturer-test/test-maker/test-maker.component.js
+++ src/app/components/lecturer-test/test-maker/test-maker.component.js
@@ -39,19 +39,65 @@ export class TestMakerComponent extends Component {
     sendTestWithKey = (json) =>{
         const testName = this.dom.getElementById("test-name").value;
         const timeLimit = this.dom.getElementById("time-limit").value;
+        const questions = this.getAllQuestions();
         const key = json.response.key;
 
         const test = {
             name: testName,
             timeLimit: timeLimit,
-            questions: [
-
-            ]
+            questions: questions
         }
 
        testsService.createTest(key, test).then((json) => console.log(json));
     }
 
+    getAllQuestions(){
+        const allQuestions = [];
+        const questionCreatorsContainer = this.dom.getElementById("question-wording-properties");
 
+        for(let question of questionCreatorsContainer.getElementsByTagName("*") )
+        {
+            const questionInfo = this.getQuestionInfo(question);
+            allQuestions.push(questionInfo);
+        }
+
+        return allQuestions;
+    }
+
+    getQuestionInfo(question){
+        //TODO: doplnte obsah funkcii na ziskavanie info z vasej otazky
+        let questionInfo;
+
+        if(question.tagName === 'APP-MULTIPLE-ANSWER-QUESTION-CREATOR'){
+            questionInfo = this.getMultiChoiceQuestionInfo(question);
+        }
+        else if(question.tagName === 'APP-MATH-QUESTION-CREATOR'){
+            questionInfo = this.getMathQuestionInfo(question);
+        }
+        else if(question.tagName === 'APP-ONE-ANSWER-QUESTION-CREATOR'){
+            questionInfo = this.getOneAnswerQuestionInfo(question);
+        }
+        else if(question.tagName === 'APP-PAIR-QUESTION-CREATOR'){
+            questionInfo = this.getPairQuestionInfo(question);
+        }
+
+        return  questionInfo;
+    }
+
+    getMultiChoiceQuestionInfo(question){
+        return {type: "multiChoice"};
+    }
+
+    getMathQuestionInfo(question){
+        return {type: "math"};
+    }
+
+    getOneAnswerQuestionInfo(question){
+        return {type: "oneAnswer"};
+    }
+
+    getPairQuestionInfo(question){
+        return {type: "oneAnswer"};
+    }
 
 }
