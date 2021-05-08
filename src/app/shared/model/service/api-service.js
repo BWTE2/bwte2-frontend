a19cf503c12d3b94176d661694e384f7db441de8
michPale <palencar.michal@gmail.com>
2021-05-08 23:44:00 +0200

    changed url

--- src/app/shared/model/service/api-service.js
+++ src/app/shared/model/service/api-service.js
@@ -10,7 +10,7 @@ export class ApiService {
     constructor() {
         const pathEncoder = new PathEncoder();
         if (pathEncoder.isLocalhost()) {
-            this.rootURL = 'https://wt143.fei.stuba.sk/bwte2/bwte2-api/';
+            this.rootURL = 'https://wt117.fei.stuba.sk/bwte2/bwte2-api/';
         } else {
             this.rootURL = '/bwte2/bwte2-api/';
         }
