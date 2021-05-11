706ec3fe85da81eed9f8322688d51bffd75be944
LubosSremanak <luboss2106@gmail.com>
2021-05-11 11:22:49 +0200

    feat: Add detail tables

--- src/app/components/lecturer-test/non-active-test-detail/non-active-test-detail.component.js
+++ src/app/components/lecturer-test/non-active-test-detail/non-active-test-detail.component.js
@@ -1,21 +1,21 @@
 import {Component} from "../../../shared/model/component/component.js";
-import {tableService} from "../../../shared/services/table.service.js";
 import {testsService} from "../../../api/tests/services/tests.service.js";
-
+import {tableService} from "../../../shared/services/table.service.js";
 
 const component = {
-    selector: 'app-active-test-detail',
-    templatePath: 'lecturer-test/active-test-detail/active-test-detail.component.html',
-    stylePaths: ['lecturer-test/active-test-detail/active-test-detail.component.css',
-        'lecturer-test/test-table/test-table.component.css'],
+    selector: 'app-non-active-test-detail',
+    templatePath: 'lecturer-test/non-active-test-detail/non-active-test-detail.component.html',
+    stylePaths: ['lecturer-test/non-active-test-detail/non-active-test-detail.component.css'
+        , 'lecturer-test/test-table/test-table.component.css'],
 };
 
-export class ActiveTestDetailComponent extends Component {
+export class NonActiveTestDetailComponent extends Component {
     static selector = component.selector;
 
     constructor() {
         super(component);
         this.load().then(() => this.onInit());
+
     }
 
     onInit() {
@@ -42,11 +42,17 @@ export class ActiveTestDetailComponent extends Component {
     };
 
     createRow = (student) => {
-        //TODO: Uprav premenne prosimta nevim ci som trafi lnazvy
+        //TODO: Uprav premenne prosimta nevim ci som trafil nazvy
         const name = tableService.getColumn(student.name + ' ' + student.surname);
         const id = tableService.getColumn(student.id);
-        // const status = tableService.getColumn(student.status);
-        const row = tableService.getRow([name, id, 'placeHolder']);
+        const action = tableService.getIconButton('editTest', 'fa-arrow-circle-right');
+        action.addEventListener("click", this.editStudentTest);
+        const row = tableService.getRow([name, id, action]);
         this.table.appendChild(row);
     };
+
+    editStudentTest = () => {
+        //TODO:JA TO DOROBIM KED VSTANEM
+    };
+
 }
