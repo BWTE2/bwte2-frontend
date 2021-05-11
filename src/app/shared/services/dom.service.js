90c639d365f8610fb74bdf6f964704ea78ddf9c5
LubosSremanak <luboss2106@gmail.com>
2021-05-11 11:23:26 +0200

    feat: Add changeDomAndSetAttribute method  in domService

--- src/app/shared/services/dom.service.js
+++ src/app/shared/services/dom.service.js
@@ -21,6 +21,26 @@ class DomService {
     }
 
     /**
+     * Change content of DOM/Element to Component and set attribute
+     *
+     * @param {any} dom - Dom or Element
+     * @param {any} component - Component
+     * @param {any} attribute - Component
+     * @return {void}
+     *
+     * @example
+     *
+     *    changeDom(domElement,UserComponent);
+     */
+    changeDomAndSetAttribute(dom, component, attribute) {
+        dom.innerHTML = "";
+        const dynamicComponent = document.createElement(component.selector);
+        this.setAttribute(dynamicComponent, attribute.name, attribute.data);
+        dom.append(dynamicComponent);
+    }
+
+
+    /**
      * Append content of DOM/Element to Component
      *
      * @param {any} dom - Dom or Element
