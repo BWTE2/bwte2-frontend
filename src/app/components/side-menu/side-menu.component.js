1a020491ffd93d0986772b7fea301df1a3e31c99
LubosSremanak <luboss2106@gmail.com>
2021-05-11 07:47:06 +0200

    feat: Add handlers for navigation change according to type - Add event listeners to open test builder, show all tests, logout and on update all tests

--- src/app/components/side-menu/side-menu.component.js
+++ src/app/components/side-menu/side-menu.component.js
@@ -20,65 +20,114 @@ export class SideMenuComponent extends Component {
     onInit() {
         this.loadLottieAnimation();
         this.attributesInitializer();
+        this.menuTypeHandler();
         this.eventsInitializer();
     }
 
     attributesInitializer() {
         this.setHeaderName();
-        this.setHeaderColor();
+
     }
 
     eventsInitializer() {
-        this.dom.getElementById("side-menu-swap-button").addEventListener("click", this.swapMenu);
-        this.dom.getElementById("send-test-button").addEventListener('click', this.sendTest);
+        try {
+            this.dom.getElementById("side-menu-swap-button").addEventListener("click", this.swapMenu);
+            this.dom.getElementById("send-test-button").addEventListener('click', this.sendTest);
+        } catch (e) {
+            this.dom.getElementById("create-test-button").addEventListener('click', this.openCreateTest);
+            this.dom.getElementById("show-all-tests").addEventListener('click', this.openShowAll);
+            this.dom.getElementById("logout-button").addEventListener('click', this.logout);
+            document.addEventListener("updateAllTests", this.openShowAll);
+        }
+
+
+    }
+
+    menuTypeHandler() {
+        this.setHeaderColor();
+        const type = this.getAttribute('type');
+        if (type === "student") {
+            domService.removeAllElementsByClass(this.dom, 'lecturer');
+        }
+        if (type === "lecturer") {
+            domService.removeAllElementsByClass(this.dom, 'student');
+        }
+
     }
 
     swapMenu = () => {
         const menu = this.dom.getElementById("side-menu");
-        const menuHeader = this.dom.getElementById("side-menu-header");
-        const menuHeaderName = this.dom.getElementById("side-menu-header-name");
         const width = menu.style.width;
         domService.createAndEmitEvent(this, "menuSwap", width);
         if (width === '0px' || width === '') {
             this.showMenu();
-            menuHeader.style.width = '400px';
-            menuHeaderName.style.display = "flex";
-            menuHeaderName.style.marginRight = "0px";
-
         } else {
             this.hideMenu();
-            menuHeader.style.width = '100px';
-            menuHeaderName.style.display = "none";
-            menuHeaderName.style.marginRight = "50px";
-
         }
     };
 
-    setAnimation = (animation) => {
-        this.animation = animation;
-    };
 
     sendTest = () => {
         domService.createAndEmitEvent(document, "sendTest", true);
     };
 
+    openCreateTest = () => {
+        this.swapCreateButton();
+        domService.createAndEmitEvent(this, "openCreateTest", true);
+    };
+
+    openShowAll = () => {
+        this.swapShowAllButton()
+        domService.createAndEmitEvent(this, "showAllTests", true);
+    };
+
+    swapShowAllButton() {
+        const all = this.dom.getElementById("show-all-tests");
+        const create = this.dom.getElementById("create-test-button");
+        all.style.display = "none";
+        create.style.display = "flex";
+    }
+
+    swapCreateButton() {
+        const all = this.dom.getElementById("show-all-tests");
+        const create = this.dom.getElementById("create-test-button");
+        create.style.display = "none";
+        all.style.display = "flex";
+    }
+
+    logout = () => {
+        domService.createAndEmitEvent(document, "logout", true);
+    };
+
     showMenu() {
-        const sideMenu = this.dom.getElementById("side-menu");
-        const header = this.dom.getElementById("side-menu-header");
-        header.style.borderBottomRightRadius = '0px';
-        sideMenu.style.width = "400px";
+        const menu = this.createMenuElements();
+        menu.header.style.borderBottomRightRadius = '0px';
+        menu.sideMenu.style.width = "400px";
+        menu.menuHeader.style.width = '400px';
+        menu.menuHeaderName.style.display = "flex";
+        menu.menuHeaderName.style.marginRight = "0px";
         this.animation.setSpeed(1.8);
         this.animation.playSegments([30, 60], true);
     }
 
     hideMenu() {
-        const sideMenu = this.dom.getElementById("side-menu");
-        const header = this.dom.getElementById("side-menu-header");
-        header.style.borderBottomRightRadius = '20px';
-        sideMenu.style.width = "0px";
+        const menu = this.createMenuElements();
+        menu.header.style.borderBottomRightRadius = '20px';
+        menu.sideMenu.style.width = "0px";
+        menu.menuHeader.style.width = '100px';
+        menu.menuHeaderName.style.display = "none";
+        menu.menuHeaderName.style.marginRight = "50px";
         this.animation.setSpeed(1.8);
         this.animation.playSegments([50, 30], true);
 
+    }
+
+    createMenuElements() {
+        const sideMenu = this.dom.getElementById("side-menu");
+        const header = this.dom.getElementById("side-menu-header");
+        const menuHeader = this.dom.getElementById("side-menu-header");
+        const menuHeaderName = this.dom.getElementById("side-menu-header-name");
+        return {sideMenu, header, menuHeader, menuHeaderName};
     }
 
     loadLottieAnimation() {
