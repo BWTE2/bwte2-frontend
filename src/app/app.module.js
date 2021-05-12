69a6beeabe5cafa0a930f551bcb1f07340a5999d
LubosSremanak <luboss2106@gmail.com>
2021-05-12 21:57:58 +0200

    feat: Add Math library

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -31,7 +31,9 @@ import {PairQuestionViewComponent} from "./components/lecturer-test/edit-test/pa
 import {OneAnswerQuestionViewComponent} from "./components/lecturer-test/edit-test/one-answer-question-view/one-answer-question-view.component.js";
 import {MultipleAnswerQuestionViewComponent} from "./components/lecturer-test/edit-test/multiple-answer-question-view/multiple-answer-question-view.component.js";
 import * as FontAwesome from "https://kit.fontawesome.com/a2f338e40d.js";
-
+import * as Jquery from "https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js";
+import * as MathQuillAll from "./shared/library/mathquill/mathquill.js";
+export const MQ = MathQuill.getInterface(2);
 
 export class AppModule {
     components = [
@@ -71,7 +73,7 @@ export class AppModule {
 
     constructor() {
         window.onload = () => this.onLoad(this.components);
-        window.FontAwesome = FontAwesome;
+        console.log("Libraries", MathQuillAll, Jquery, FontAwesome);
     }
 
     onLoad(components) {
--- src/app/components/lecturer-test/test-maker/math-question-creator/math-question-creator.component.js
+++ src/app/components/lecturer-test/test-maker/math-question-creator/math-question-creator.component.js
@@ -1,10 +1,12 @@
 import {Component} from "../../../../shared/model/component/component.js";
+import {MQ} from "../../../../app.module.js";
 
 
 const component = {
     selector: 'app-math-question-creator',
     templatePath: 'lecturer-test/test-maker/math-question-creator/math-question-creator.component.html',
-    stylePaths: ['lecturer-test/test-maker/math-question-creator/math-question-creator.component.css'],
+    stylePaths: ['lecturer-test/test-maker/math-question-creator/math-question-creator.component.css',
+        '../../app/shared/library/mathquill/mathquill.css'],
 };
 
 export class MathQuestionCreatorComponent extends Component {
@@ -21,7 +23,8 @@ export class MathQuestionCreatorComponent extends Component {
     }
 
     attributesInitializer() {
-
+        let problemSpan = this.dom.getElementById('problem');
+        MQ.StaticMath(problemSpan);
     }
 
     eventsInitializer() {
