de385155113ab5e07c6065f1e7139bbfbe5ca177
LubosSremanak <luboss2106@gmail.com>
2021-05-16 20:07:23 +0200

    feat: Add math question image upload

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -33,12 +33,11 @@ import {MultipleAnswerQuestionViewComponent} from "./components/lecturer-test/ed
 import {MathPanelComponent} from "./components/math-panel/math-panel.component.js";
 import {PointsEditComponent} from "./components/lecturer-test/edit-test/points-edit/points-edit.component.js";
 import {CanvasComponent} from "./components/student-test/draw-question/canvas/canvas.component.js";
-import {QrCodeComponent} from "./components/qr-code/qr-code.component.js";
-import {QrMobileComponent} from "./components/qr-mobile/qr-mobile.component.js";
 import * as FontAwesome from "https://kit.fontawesome.com/a2f338e40d.js";
 import * as Jquery from "https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js";
 import * as MathQuillAll from "./shared/library/mathquill/mathquill.js";
 import {CanvasPanelComponent} from "./components/student-test/draw-question/canvas/canvas-panel/canvas-panel.component.js";
+import {DocumentationComponent} from "./components/documentation/documentation.component.js";
 
 
 export const MQ = MathQuill.getInterface(2);
@@ -81,8 +80,7 @@ export class AppModule {
         PointsEditComponent,
         CanvasComponent,
         CanvasPanelComponent,
-        QrCodeComponent,
-        QrMobileComponent
+        DocumentationComponent
     ]
 
     constructor() {
--- src/app/components/lecturer-test/edit-test/math-question-view/math-question-view.component.js
+++ src/app/components/lecturer-test/edit-test/math-question-view/math-question-view.component.js
@@ -7,7 +7,8 @@ const component = {
     selector: 'app-math-question-view',
     templatePath: 'lecturer-test/edit-test/math-question-view/math-question-view.component.html',
     stylePaths: ['lecturer-test/edit-test/math-question-view/math-question-view.component.css',
-        '../../app/shared/library/mathquill/mathquill.css'],
+        '../../app/shared/library/mathquill/mathquill.css', 'student-test/draw-question/draw-question.component.css',
+        'lecturer-test/edit-test/draw-question-view/draw-question-view.component.css'],
 };
 
 export class MathQuestionViewComponent extends Component {
@@ -25,23 +26,26 @@ export class MathQuestionViewComponent extends Component {
 
     attributesInitializer() {
 
-        const test = domService.getAttribute(this, "test");
-        this.questionId = test.question.id;
-
-        this.preloadPoints(test.question.points, test.question.answer);
-        this.loadQuestionWording(test.question);
+        this.test = domService.getAttribute(this, "test");
+        this.questionId = this.test.question.id;
+        this.dom.getElementById("open-picture").addEventListener("click", this.openPicture);
+        this.preloadPoints(this.test.question.points, this.test.question.answer);
+        this.loadQuestionWording(this.test.question);
     }
 
     eventsInitializer() {
     }
 
     preloadPoints(points, answer) {
+
         const pointsEdit = this.dom.getElementById("points-edit");
         domService.setAttribute(pointsEdit, "points", points);
 
-        const answerBox = this.dom.getElementById("student-answer");
-        answerBox.innerText = answer;
-        MQ.StaticMath( answerBox );
+        if (answer.split("\picture").length < 2) {
+            const answerBox = this.dom.getElementById("student-answer");
+            answerBox.innerText = answer;
+            MQ.StaticMath(answerBox);
+        }
     }
 
     loadQuestionWording(question) {
@@ -54,7 +58,7 @@ export class MathQuestionViewComponent extends Component {
         }
 
         let newText = document.createElement("p");
-        newText.innerText= textMath[1];
+        newText.innerText = textMath[1];
         MQ.StaticMath(newText);
         this.dom.getElementById("math-exp").appendChild(newText);
         domService.setAttribute(questionWordingElement, "questionWording", questionWording);
@@ -64,4 +68,27 @@ export class MathQuestionViewComponent extends Component {
         const pointsEdit = this.dom.getElementById("points-edit");
         return {points: pointsEdit.getPoints(), questionId: this.questionId};
     }
+
+    openPicture = () => {
+        const container = document.createElement('div');
+        container.id = "modal-image-container";
+        const apiUrl = "https://latex.codecogs.com/png.download?%5Cdpi%7B300%7D%20%5Cfn_phv%20%5Chuge";
+        const image = document.createElement('img');
+        const answerType = this.test.question.answer.split("\picture");
+        if (answerType.length < 2) {
+            image.setAttribute("src", apiUrl + encodeURI(this.test.question.answer));
+
+        } else {
+            image.setAttribute("src", answerType[1]);
+        }
+        image.id = "modal-image";
+        container.appendChild(image);
+        container.addEventListener("click", this.closeImageView);
+        this.dom.appendChild(container);
+    }
+
+    closeImageView = () => {
+        this.dom.getElementById('modal-image-container').remove();
+    };
+
 }
