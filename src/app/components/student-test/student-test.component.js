d4da43c0b3b1734a9117f48b5c2b7b236af3d86b
LubosSremanak <luboss2106@gmail.com>
2021-05-16 07:20:42 +0200

    refactor: Remove alert after bad request

--- src/app/components/student-test/student-test.component.js
+++ src/app/components/student-test/student-test.component.js
@@ -28,8 +28,6 @@ export class StudentTestComponent extends Component {
         this.preResponse = await testsService.readQuestions(testKey);
 
         if (this.preResponse.responseErrorMessage) {
-            alert(this.preResponse.responseErrorMessage.responseCode)
-            console.log(this.preResponse);
             this.handleErrorResponseMessage(this.preResponse.responseErrorMessage);
         }
     }
